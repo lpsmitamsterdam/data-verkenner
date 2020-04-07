8c8f642842702c8e1204742e26f40d0abd9b5eab
Jon Koops <jonkoops@gmail.com>
2020-04-07 11:08:05 +0200

    Fix linting issues

--- src/app/utils/getLoadingErrors.test.ts
+++ src/app/utils/getLoadingErrors.test.ts
@@ -4,10 +4,12 @@ import getLoadingErrors from './getLoadingErrors'
 
 describe('getLoadingErrors', () => {
   it('should get the loading errors', () => {
-    const ERROR_UNAUTHORIZED =
-      { extensions: { code: 'UNAUTHORIZED' } } as GraphQLFormattedError<ErrorExtensions>
-    const ERROR_DIFFERENT_CODE =
-      { extensions: { code: 'FOO' } } as GraphQLFormattedError<ErrorExtensions>
+    const ERROR_UNAUTHORIZED = { extensions: { code: 'UNAUTHORIZED' } } as GraphQLFormattedError<
+      ErrorExtensions
+    >
+    const ERROR_DIFFERENT_CODE = { extensions: { code: 'FOO' } } as GraphQLFormattedError<
+      ErrorExtensions
+    >
     const ERROR_NO_EXTENSIONS = {} as GraphQLFormattedError<ErrorExtensions>
 
     expect(
