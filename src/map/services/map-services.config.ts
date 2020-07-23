34a2b667c927d8fc4724a872b4bec6ebe226ab3b
royderks <10717410+royderks@users.noreply.github.com>
2020-07-23 15:44:45 +0200

    Updated detail info of the bouwstroom maplayer panel

--- src/map/services/map-services.config.ts
+++ src/map/services/map-services.config.ts
@@ -386,8 +386,20 @@ const servicesByEndpointType: { [type: string]: ServiceDefinition } = {
               description: result.beschikbareAansluitingen.map((item: string) => item).join('\n'),
             },
             {
-              term: 'Beheerdersdeel',
+              term: 'Beheerorganisatie',
               description: result.organisatie || 'onbekend',
+            },
+            {
+              term: 'Contactgegevens',
+              description: result.email || 'onbekend',
+            },
+            {
+              term: 'Licentie benodigd',
+              description: result.vergunningsplichtig ? 'Ja' : 'Nee',
+            },
+            {
+              term: 'Adresgegevens',
+              description: `${result.straat} ${result.huisnummer}, ${result.plaats}`,
             },
           ].filter(hasDescription),
         },
