4423fbf5c57e3268ffd9073eb1b197fc93e1f9d1
Jon Koops <jonkoops@gmail.com>
2020-09-11 10:52:02 +0200

    Prevent focus from being changed when navigating between routes

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,22 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-import createSagaMiddleware from 'redux-saga'
 import { createBrowserHistory, createMemoryHistory } from 'history'
+import queryString from 'querystring'
+import { applyMiddleware, compose, createStore } from 'redux'
 import { connectRoutes } from 'redux-first-router'
 import restoreScroll from 'redux-first-router-restore-scroll'
-import queryString from 'querystring'
+import createSagaMiddleware from 'redux-saga'
+import routes from '../app/routes'
+import rootReducer from '../reducers/root'
 import rootSaga from '../root-saga'
 import '../shared/ducks/error/error-message'
-import * as auth from '../shared/services/auth/auth'
 import { authenticateReload } from '../shared/ducks/user/user'
-import rootReducer from '../reducers/root'
+import * as auth from '../shared/services/auth/auth'
+import urlParamsMiddleware from './middleware/addMetaToRoutesMiddleware'
 import documentHeadMiddleware from './middleware/documentHead'
 import matomoMiddleware from './middleware/matomo/matomoMiddleware'
-import urlParamsMiddleware from './middleware/addMetaToRoutesMiddleware'
 import preserveUrlParametersMiddleware from './middleware/preserveUrlParametersMiddleware'
 import setQueriesFromStateMiddleware from './middleware/setQueriesFromStateMiddleware'
 import paramsRegistry from './params-registry'
 import './queryParameters'
-import routes from '../app/routes'
 
 const configureStore = (storybook = false, req) => {
   const routesMap = routes
@@ -71,15 +71,6 @@ const configureStore = (storybook = false, req) => {
   }
 
   const enhancer = composeEnhancers(routeEnhancer, applyMiddleware(...middleware))
-
-  // Element that have a focus should blur on a route change. This prevents issues like
-  // having a header menu open when the user navigates
-  history.listen(() => {
-    if (document.activeElement) {
-      document.activeElement.blur()
-    }
-  })
-
   const store = createStore(rootReducer(routeReducer), undefined, enhancer)
 
   if (typeof window !== 'undefined') {
