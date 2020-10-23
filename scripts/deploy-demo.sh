503b3143b99ac30f743b882bf44ef90fc3d32db2
Chris van Mook <chris@chrisvanmook.com>
2020-10-23 15:45:59 +0200

    Improve deploy-demo script + support for older git versions (#3500)

--- scripts/deploy-demo.sh
+++ scripts/deploy-demo.sh
@@ -10,12 +10,6 @@ fi
 SOURCE_BRANCH=$(git branch --show-current)
 TARGET_BRANCH="demo$SEVER_NUMBER"
 
-echo "Pushing $SOURCE_BRANCH to $TARGET_BRANCH."
-
-git branch -D $TARGET_BRANCH
-git checkout -b $TARGET_BRANCH
-
-git push --no-verify --force --set-upstream origin $TARGET_BRANCH
-git checkout $SOURCE_BRANCH
+git push origin +$SOURCE_BRANCH:$TARGET_BRANCH
 
 echo "Done. Your branch will be deployed to https://demo$SEVER_NUMBER.data.amsterdam.nl soon."
