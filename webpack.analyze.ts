29e03973b47168f3e3802b0ed22b4e7d511adf95
Jon Koops <jonkoops@gmail.com>
2020-11-03 17:22:21 +0100

    Fix Webpack bundle analyzer script

--- webpack.analyze.ts
+++ webpack.analyze.ts
@@ -2,18 +2,15 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import { merge } from 'webpack-merge'
 import configs from './webpack.prod'
 
-export default (env: { [key: string]: string }) => {
-  const configName = env.config ?? 'modern'
-  const config = configs.find(({ name }) => name === configName)
+const config = configs.find(({ name }) => name === 'modern')
 
-  if (!config) {
-    throw new Error(`Unable to find configuration named '${configName}'.`)
-  }
-
-  return merge(config, {
-    plugins: [new BundleAnalyzerPlugin()],
-    optimization: {
-      concatenateModules: false,
-    },
-  })
+if (!config) {
+  throw new Error('Unable to find configuration to analyze.')
 }
+
+export default merge(config, {
+  plugins: [new BundleAnalyzerPlugin()],
+  optimization: {
+    concatenateModules: false,
+  },
+})
