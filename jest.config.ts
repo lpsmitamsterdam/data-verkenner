3379cfcab764d4c17d6827b63f5e71faf7dc3a7c
Jon Koops <jonkoops@gmail.com>
2021-03-30 18:13:03 +0200

    Add tests for ActionButton component (#4067)

--- jest.config.ts
+++ jest.config.ts
@@ -17,10 +17,10 @@ const config: Config.InitialOptions = {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      statements: 71.03,
-      branches: 48.65,
-      lines: 70.34,
-      functions: 58.25,
+      statements: 71.15,
+      branches: 48.69,
+      lines: 70.44,
+      functions: 58.37,
     },
   },
   coverageReporters: process.env.CI ? ['text'] : ['lcov'],
---
+++ src/app/components/ActionButton/ActionButton.test.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import ActionButton from './ActionButton'
+
+const LABEL = 'Some label'
+
+describe('ActionButton', () => {
+  it('renders the button', () => {
+    const { container } = render(
+      <ActionButton fetching={false} iconLeft={null} label={LABEL} onClick={() => {}} />,
+    )
+
+    expect(container.firstChild).toBeDefined()
+  })
+
+  it('passes along the other props', () => {
+    render(
+      <ActionButton
+        data-testid="actionButton"
+        fetching={false}
+        iconLeft={null}
+        label={LABEL}
+        onClick={() => {}}
+      />,
+    )
+
+    expect(screen.getByTestId('actionButton')).toBeDefined()
+  })
+
+  it('disables the button when fetching', () => {
+    render(<ActionButton fetching iconLeft={null} label={LABEL} onClick={() => {}} />)
+
+    expect(screen.getByText(LABEL)).toHaveAttribute('disabled')
+  })
+
+  it('triggers the onClick prop if pressed', () => {
+    const onClickMock = jest.fn()
+
+    render(<ActionButton fetching={false} iconLeft={null} label={LABEL} onClick={onClickMock} />)
+
+    fireEvent.click(screen.getByText(LABEL))
+
+    expect(onClickMock).toHaveBeenCalled()
+  })
+})
