54cddfce3cef57573896acdef96d85133772a32b
Jon Koops <jonkoops@gmail.com>
2021-05-14 13:04:09 +0200

    Convert useCompare() and usePrevious to TypeScript (#4193)

--- src/app/utils/usePrevious.ts
+++ src/app/utils/usePrevious.ts
@@ -1,11 +1,11 @@
 import { useEffect, useRef } from 'react'
 
-const usePrevious = (value) => {
-  const ref = useRef()
+export default function usePrevious<T>(value: T) {
+  const ref = useRef<T>()
+
   useEffect(() => {
     ref.current = value
   })
+
   return ref.current
 }
-
-export default usePrevious
