ad2c42ff7a4a826e5ab2455c94d38ab330e5be6a
lpsmitamsterdam <88381082+lpsmitamsterdam@users.noreply.github.com>
2021-08-17 13:05:23 +0200

    Feature/scss dataselection refactor (#4585)
    * replace sass table with asc styled component
    
    * add hover styles
    
    * remove old stylesheet

--- src/app/components/DataSelection/DataSelectionTable.tsx
+++ src/app/components/DataSelection/DataSelectionTable.tsx
@@ -1,56 +1,82 @@
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 import styled from 'styled-components'
 import type { FunctionComponent } from 'react'
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableContainer,
+  Link,
+  themeColor,
+} from '@amsterdam/asc-ui'
 import getDetailPageData from '../../utils/getDetailPageData'
 import { toDataDetail } from '../../links'
 import DataSelectionFormatter from './DataSelectionFormatter/DataSelectionFormatter'
 import type { Data } from './types'
 
-const TableRowLink = styled(Link)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+const StyledTableCell = styled(TableCell)`
+  white-space: nowrap;
 `
-
+const StyledTableRow = styled(TableRow)`
+  &:hover {
+    background-color: ${themeColor('tint', 'level3')};
+  }
+`
 export interface DataSelectionTableProps {
   content: Data
 }
 
 const DataSelectionTable: FunctionComponent<DataSelectionTableProps> = ({ content }) =>
   content.body.length > 0 ? (
-    <table className="c-ds-table" data-testid="dataSelectionTable">
-      <thead className="c-ds-table__head">
-        <tr className="c-ds-table__row c-ds-table__row--link">
-          {content.head.map((field) => (
-            <th key={field} className="c-ds-table__cell" scope="col">
-              {field}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody className="c-ds-table__body">
-        {content.body.map((row) => (
-          <tr
-            key={row.id}
-            className="c-ds-table__row c-ds-table__row--link qa-table-link"
-            data-testid="dataSelectionTableRow"
-          >
-            {row.content.map((variables, index) => (
-              <td key={variables[0].id} className="c-ds-table__cell">
-                <DataSelectionFormatter
-                  variables={variables}
-                  formatter={content.formatters[index]}
-                  template={content.templates[index]}
-                />
-              </td>
+    <TableContainer>
+      <Table data-testid="dataSelectionTable">
+        <TableHeader>
+          <TableRow>
+            {content.head.map((field) => (
+              <StyledTableCell as="th" key={field} scope="col">
+                {field}
+              </StyledTableCell>
             ))}
-            <TableRowLink to={toDataDetail(getDetailPageData(row.detailEndpoint as string))} />
-          </tr>
-        ))}
-      </tbody>
-    </table>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {content.body.map((row) => (
+            <StyledTableRow key={row.id} data-testid="dataSelectionTableRow">
+              {row.content.map((variables, index) => {
+                if (index === 0) {
+                  return (
+                    <StyledTableCell key={variables[0].id}>
+                      <Link
+                        variant="inline"
+                        as={RouterLink}
+                        to={toDataDetail(getDetailPageData(row.detailEndpoint as string))}
+                      >
+                        <DataSelectionFormatter
+                          variables={variables}
+                          formatter={content.formatters[index]}
+                          template={content.templates[index]}
+                        />
+                      </Link>
+                    </StyledTableCell>
+                  )
+                }
+                return (
+                  <StyledTableCell key={variables[0].id}>
+                    <DataSelectionFormatter
+                      variables={variables}
+                      formatter={content.formatters[index]}
+                      template={content.templates[index]}
+                    />
+                  </StyledTableCell>
+                )
+              })}
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   ) : null
 
 export default DataSelectionTable
